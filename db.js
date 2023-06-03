3137cf1cd4ddbd740f295128518aa76fd85de8a9
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-06-03 12:58:51 +0200

    feat(db):create a function for query login

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2");
 
+
+
 const db = mysql.createConnection({
     host: "localhost",
     port: 8889,
@@ -7,7 +9,17 @@ const db = mysql.createConnection({
     password: "root",
     database: "ocni"
 });
-
+async function validateUser(email, password) {
+    // Recupera l'utente dal database in base all'email
+    const user = await getUserByEmail(email)
+    
+    // Verifica la password
+    const isValid = bcrypt.compare(password, user.password)
+    if (isValid) {
+      return user
+    }  
+    return null
+  }
 db.connect();
 // console.log(db);
 console.log('DB CONNECTED');
